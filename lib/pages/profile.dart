82a3e484c1f0275da9fd0737a99e967861fbf991
Heni Rekik <heni.rekik8@gmail.com>
2022-08-02 19:11:58 +0200

    'headerProfile'

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fluttershare/widgets/header.dart';
 
 class Profile extends StatefulWidget {
   @override
   _ProfileState createState() => _ProfileState();
 }
-
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Text("Profile");
+    //return Text("Profile");
+    return Scaffold(
+      appBar: header(context, titleText: "Profile"),
+      body: Text('Profile'),
+    );
   }
 }
--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fluttershare/widgets/header.dart';
 
 class Timeline extends StatefulWidget {
   @override
   _TimelineState createState() => _TimelineState();
 }
-
 class _TimelineState extends State<Timeline> {
   @override
   Widget build(context) {
-    return Text("Timeline");
+    //return Text("Timeline");
+    return Scaffold(
+      appBar: header(context, isAppTitle: true, titleText: ''),
+      body: Text('Timeline'),
+    );
   }
 }
